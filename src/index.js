cce181a94d850227df6e761eaff9a0b785782c17
GromovaJ <gromovajji@gmail.com>
2018-03-27 23:52:40 +0300

    correct ESLint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
-const get_invite = () => {
+
+const getInvite = () => {
   console.log('Welcome to the Brain Games!');
   console.log('');
   const actual = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${actual}!`);
-  return actual
+  return actual;
 };
-export const invite = get_invite();
+const invite = getInvite();
