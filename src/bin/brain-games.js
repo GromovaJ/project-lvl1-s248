72e3ac3a8fdc791275ee5d55d6bc186ce1945dc7
GromovaJ <gromovajji@gmail.com>
2018-03-28 11:45:36 +0300

    fix bugs step2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
-import invite from '..';
+import { getInvite } from '..';
+getInvite();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const getInvite = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('');
-  const actual = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${actual}!`);
-  return actual;
+export const getInvite = () => {
+  console.log('Welcome to the Brain Games!\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
-export const invite = getInvite();
