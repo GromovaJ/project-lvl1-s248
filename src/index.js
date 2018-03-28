b53c69590aa01b4e7499a3cae44d1b4daa8cd496
GromovaJ <gromovajji@gmail.com>
2018-03-28 12:14:32 +0300

    fix bugs step3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const getInvite = () => {
+const getInvite = () => {
   console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+export default getInvite();
