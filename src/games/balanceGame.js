2cd694ccbaa0b3991fdf9a0f22a1dc439d65639e
GromovaJ <gromovajji@gmail.com>
2018-03-31 09:56:22 +0300

    fixes step7

--- src/games/balanceGame.js
+++ src/games/balanceGame.js
@@ -1,33 +1,11 @@
 import { cons } from 'hexlet-pairs';
 import { getGame } from '..';
-import { getRandomNumber, compareNumeric } from '../general';
-
-
-const getCalc = (num) => {
-  const newArr = [];
-  const numLength = String(num).length;
-  const arr = String(num).split('').map(Number);
-  arr.sort(compareNumeric);
-  if (arr[numLength - 1] - arr[0] <= 1) return String(num);
-  const numSum = arr.reduce((sum, current) => {
-    const ss = sum + current;
-    return ss;
-  }, 0);
-  const remainder = numSum % numLength;
-  const digit = Math.floor(numSum / numLength);
-  for (let counter = 0; counter < numLength; counter += 1) {
-    if (counter <= numLength - remainder - 1) {
-      newArr.push(digit);
-    } else newArr.push(digit + 1);
-  }
-  const newString = newArr.join('');
-  return newString;
-};
+import { getRandomNumber, getBalancedNumber } from '../general';
 
 const getData = () => {
-  const x = getRandomNumber();
-  const y = getCalc(x);
-  return cons(x, y);
+  const question = getRandomNumber();
+  const answer = getBalancedNumber(question);
+  return cons(question, answer);
 };
 
 export default () => {
--- src/general.js
+++ src/general.js
@@ -1,5 +1,5 @@
 export const getRandomNumber = () => {
-  const randomNumber = Math.floor((Math.random() * 10) + 1);
+  const randomNumber = Math.floor((Math.random() * 1000) + 1);
   return randomNumber;
 };
 
@@ -37,4 +37,38 @@ export const getGcd = (num1, num2) => {
 export const compareNumeric = (a, b) => {
   if (a > b) return 1;
   return -1;
+};
+
+export const getBalancedNumber = (num) => {
+  const newArr = [];
+  const lengthOfNumber = String(num).length;
+  const arr = String(num).split('').map(Number);
+
+  arr.sort(compareNumeric);
+
+  if (arr[lengthOfNumber - 1] - arr[0] <= 1) return String(num);
+  const sumOfDigitsOfNumber = arr.reduce((sum, current) => {
+    const ss = sum + current;
+    return ss;
+  }, 0);
+
+  const remainder = sumOfDigitsOfNumber % lengthOfNumber;
+  const digitOfBalancedNumber = Math.floor(sumOfDigitsOfNumber / lengthOfNumber);
+
+  for (let counter = 0; counter < lengthOfNumber; counter += 1) {
+    if (counter <= lengthOfNumber - remainder - 1) {
+      newArr.push(digitOfBalancedNumber);
+    } else newArr.push(digitOfBalancedNumber + 1);
+  }
+  const balancedNumberString = newArr.join('');
+  return balancedNumberString;
+};
+
+export const isPrime = (num) => {
+  for (let counter = 2; counter <= Math.floor(Math.sqrt(num + 1)); counter += 1) {
+    if (num % counter === 0) {
+      return false;
+    }
+  }
+  return true;
 };
